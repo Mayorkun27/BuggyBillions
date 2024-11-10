53fc985bb67bbd4327c9fa7b4b2e24630eba09c4
SeniorCub <farinderif@gmail.com>
2024-11-10 16:17:07 +0100

    Update Tailwind configuration and styles; add license to package-lock

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        pryPurple : "#796fab",
-        secBlack : "#282828",
-        secWhite : "#eff6f8",
-        accYellow : "#e5aa2d",
-        accPink : "#e487bc",
-      },
-    },
-  },
-  plugins: [],
-}
+     content: [
+       "./**/*.{html,js}",  // All HTML and JS files in the root and any subfolders
+       "./src/**/*.{html,js}" // All HTML and JS files specifically inside the src folder and its subfolders
+     ],
+     theme: {
+       extend: {
+         colors: {
+           pryPurple: "#796fab",
+           secBlack: "#282828",
+           secWhite: "#eff6f8",
+           accYellow: "#e5aa2d",
+           accPink: "#e487bc",
+         },
+       },
+     },
+     plugins: [],
+   }   
