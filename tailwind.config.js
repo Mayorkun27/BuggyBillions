1d19a7dd6acab7484f13679329b7a15520e2c8ad
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-11-11 14:03:00 +0100

    Progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,26 @@ module.exports = {
       boxShadow: {
         'custom-shadow': '10px 5px 20px #00000032, -10px -5px 30px #00000025',
       },
+      textShadow: {
+        sm: '1px 1px 2px rgba(0, 0, 0, 0.5)',
+        DEFAULT: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        lg: '3px 3px 6px rgba(0, 0, 0, 0.5)',
+        xl: '-5px 5px 2px #000000af',
+        none: 'none',
+      },
     },
   },
   plugins: [
-    // require('flowbite/plugin')
+    require('flowbite/plugin'),
+    function ({ addUtilities, theme, e }) {
+      const textShadow = theme('textShadow');
+      const utilities = Object.entries(textShadow).map(([key, value]) => ({
+        [`.${e(`text-shadow-${key}`)}`]: {
+          textShadow: value,
+        },
+      }));
+
+      addUtilities(utilities);
+    },
   ],
 }
